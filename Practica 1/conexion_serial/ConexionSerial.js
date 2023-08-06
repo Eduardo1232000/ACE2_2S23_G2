2d22a68fb7021fca66f21a144c60877cd8c02f0c
ESTUARDO SON <2986622050101@ingenieria.usac.edu.gt>
2023-08-06 17:32:52 -0600

    [ADD] Base de Conexion Serial
    Se agrego una primera versión de lo que será la conexión de arduino y la base de datos por medio de un programa en Nodejs

---
+++ Practica 1/conexion_serial/ConexionSerial.js
@@ -0,0 +1,40 @@
+// Importación de librerias para conexion serial
+const SerialPort = require("serialport").SerialPort;
+const ReadLineParser = require("serialport").ReadlineParser;
+
+// Importación de librerias para conexion a base de datos
+const { getConnection } = require("./ConexionDB");
+const connection = getConnection();
+
+// Creación de objeto puerto para comunicación serial
+const puerto = new SerialPort({
+  path: 'COM3',
+  baudRate: 9600,
+});
+
+// Creación de objeto parser para lectura de datos
+const parser = puerto.pipe(new ReadLineParser())
+
+// Evento de conexion abierta
+parser.on("open", () => {
+  console.log("Conexion abierta");
+});
+
+// Evento de recepción de datos
+parser.on("data", (data) => {
+  // Insertar datos en base de datos
+  connection.query(`INSERT INTO prueba VALUES (?);`, [data],(error, results) => {
+    if (error) { 
+      // En caso de que haya error
+      console.log("Error al insertar" + error);
+    } else {
+      // En caso de que no haya error
+      console.log("Insertado correctamente");
+    }
+  });
+});
+
+// Evento de error
+puerto.on("error", (err) => {
+  console.log(err);
+});
