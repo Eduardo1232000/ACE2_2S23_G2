eb8e066fa429128838831dc1e0d75f00f1e4fd7d
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2023-08-15 17:38:10 -0600

    [UPDATE] Se ha actualizado el funcionamiento de la grafica radial debido a la nueva forma en la que se toman datos de la dirección del viento

--- Practica 1/dashboard/src/index.js
+++ Practica 1/dashboard/src/index.js
@@ -94,7 +94,18 @@ router.get("/informacion_radial/:fecha_desde/:fecha_hasta", (req, res) => {
   if (fecha_desde == fecha_hasta) {
     req.getConnection((err, conn) => {
       const sql = `SELECT direccion_viento, DATE_FORMAT(fecha_registro, '%H:00:00') AS hora, COUNT(direccion_viento) AS prom_direccion_viento
-      FROM medicion
+      FROM
+      (SELECT CASE
+        WHEN direccion_viento >= 0 AND direccion_viento < 45 THEN 'N'
+          WHEN direccion_viento >= 45 AND direccion_viento < 90 THEN 'NE'
+          WHEN direccion_viento >= 90 AND direccion_viento < 135 THEN 'E'
+          WHEN direccion_viento >= 135 AND direccion_viento < 180 THEN 'SE'
+          WHEN direccion_viento >= 180 AND direccion_viento < 225 THEN 'S'
+          WHEN direccion_viento >= 225 AND direccion_viento < 270 THEN 'SO'
+          WHEN direccion_viento >= 270 AND direccion_viento < 315 THEN 'O'
+          WHEN direccion_viento >= 315 AND direccion_viento < 360 THEN 'NO'
+      END AS direccion_viento, fecha_registro
+      FROM medicion m) AS query
       WHERE fecha_registro >= '` + fecha_desde + `' AND fecha_registro < DATE_ADD('` + fecha_hasta + `', INTERVAL 1 DAY)
       GROUP BY direccion_viento, DATE_FORMAT(fecha_registro, '%H:00:00')
       ORDER BY direccion_viento, hora ASC;`;
@@ -109,7 +120,18 @@ router.get("/informacion_radial/:fecha_desde/:fecha_hasta", (req, res) => {
   } else {
     req.getConnection((err, conn) => {
       const sql = `SELECT direccion_viento, DATE_FORMAT(fecha_registro, '%d/%m') AS dia, COUNT(direccion_viento) AS prom_direccion_viento
-      FROM medicion
+      FROM
+      (SELECT CASE
+        WHEN direccion_viento >= 0 AND direccion_viento < 45 THEN 'N'
+          WHEN direccion_viento >= 45 AND direccion_viento < 90 THEN 'NE'
+          WHEN direccion_viento >= 90 AND direccion_viento < 135 THEN 'E'
+          WHEN direccion_viento >= 135 AND direccion_viento < 180 THEN 'SE'
+          WHEN direccion_viento >= 180 AND direccion_viento < 225 THEN 'S'
+          WHEN direccion_viento >= 225 AND direccion_viento < 270 THEN 'SO'
+          WHEN direccion_viento >= 270 AND direccion_viento < 315 THEN 'O'
+          WHEN direccion_viento >= 315 AND direccion_viento < 360 THEN 'NO'
+      END AS direccion_viento, fecha_registro
+      FROM medicion m) AS query
       WHERE fecha_registro >= '` + fecha_desde + `' AND fecha_registro < DATE_ADD('` + fecha_hasta + `', INTERVAL 1 DAY)
       GROUP BY direccion_viento, DATE_FORMAT(fecha_registro, '%d/%m')
       ORDER BY direccion_viento, dia ASC;`;
@@ -189,93 +211,88 @@ function formatearPorHoraRadial(result) {
   let labels = [];
   let data_este = [];
   let data_norte = [];
+  let data_noreste = [];
+  let data_noroeste = [];
   let data_oeste = [];
   let data_sur = [];
+  let data_sureste = [];
+  let data_suroeste = [];
 
   const este = result.filter(element => element['direccion_viento'] === 'E');
   const norte = result.filter(element => element['direccion_viento'] === 'N');
+  const noreste = result.filter(element => element['direccion_viento'] === 'NE');
+  const noroeste = result.filter(element => element['direccion_viento'] === 'NO');
   const oeste = result.filter(element => element['direccion_viento'] === 'O');
   const sur = result.filter(element => element['direccion_viento'] === 'S');
+  const sureste = result.filter(element => element['direccion_viento'] === 'SE');
+  const suroeste = result.filter(element => element['direccion_viento'] === 'SO');
 
   for (let i = 0; i < 24; i++) {
     labels.push(calcularHora(i));
-
-    const val_este = este.find(element => element['hora'] === calcularHora(i));
-    if (val_este === undefined) {
-      data_este.push(0);
-    } else {
-      data_este.push(val_este.prom_direccion_viento);
-    }
-
-    const val_norte = norte.find(element => element['hora'] === calcularHora(i));
-    if (val_norte === undefined) {
-      data_norte.push(0);
-    } else {
-      data_norte.push(val_norte.prom_direccion_viento);
-    }
-
-    const val_oeste = oeste.find(element => element['hora'] === calcularHora(i));
-    if (val_oeste === undefined) {
-      data_oeste.push(0);
-    } else {
-      data_oeste.push(val_oeste.prom_direccion_viento);
-    }
-
-    const val_sur = sur.find(element => element['hora'] === calcularHora(i));
-    if (val_sur === undefined) {
-      data_sur.push(0);
-    } else {
-      data_sur.push(val_sur.prom_direccion_viento);
-    }
+    almacenarRegistroHora(este, data_este, i);
+    almacenarRegistroHora(norte, data_norte, i);
+    almacenarRegistroHora(noreste, data_noreste, i);
+    almacenarRegistroHora(noroeste, data_noroeste, i);
+    almacenarRegistroHora(oeste, data_oeste, i);
+    almacenarRegistroHora(sur, data_sur, i);
+    almacenarRegistroHora(sureste, data_sureste, i);
+    almacenarRegistroHora(suroeste, data_suroeste, i);
   }
   return {
     labels: labels,
-    direccion: { 0: data_este, 1: data_norte, 2: data_oeste, 3: data_sur }
+    direccion: { 0: data_este, 1: data_norte, 2: data_noreste, 3: data_noroeste, 4: data_oeste, 5: data_sur, 6: data_sureste, 7: data_suroeste }
   };
 }
 
+let labelsNoRepetidos;
 function formatearPorDiaRadial(result) {
 
   let labels = [];
   let data_este = [];
   let data_norte = [];
+  let data_noreste = [];
+  let data_noroeste = [];
   let data_oeste = [];
   let data_sur = [];
+  let data_sureste = [];
+  let data_suroeste = [];
 
   const este = result.filter(element => element['direccion_viento'] === 'E');
   const norte = result.filter(element => element['direccion_viento'] === 'N');
+  const noreste = result.filter(element => element['direccion_viento'] === 'NE');
+  const noroeste = result.filter(element => element['direccion_viento'] === 'NO');
   const oeste = result.filter(element => element['direccion_viento'] === 'O');
   const sur = result.filter(element => element['direccion_viento'] === 'S');
+  const sureste = result.filter(element => element['direccion_viento'] === 'SE');
+  const suroeste = result.filter(element => element['direccion_viento'] === 'SO');
 
-  este.forEach(element => {
-    labels.push(element.dia);
-    data_este.push(element.prom_direccion_viento);
-  });
-
-  norte.forEach(element => {
-    labels.push(element.dia);
-    data_norte.push(element.prom_direccion_viento);
-  });
-
-  oeste.forEach(element => {
-    labels.push(element.dia);
-    data_oeste.push(element.prom_direccion_viento);
-  });
-
-  sur.forEach(element => {
-    labels.push(element.dia);
-    data_sur.push(element.prom_direccion_viento);
-  });
-
-  let labelsNoRepetidos = labels.filter((value, index, self) => {
+  // Se determina el label del grafico
+  extraerDias(este, labels);
+  extraerDias(norte, labels);
+  extraerDias(noreste, labels);
+  extraerDias(noroeste, labels);
+  extraerDias(oeste, labels);
+  extraerDias(sur, labels);
+  extraerDias(sureste, labels);
+  extraerDias(suroeste, labels);
+  const labelsOrdenados = labels.sort(compararFechas);
+  labelsNoRepetidos = labelsOrdenados.filter((value, index, self) => {
     return self.indexOf(value) === index;
   });
 
-  const labelsOrdenados = labelsNoRepetidos.sort(compararFechas);
+  // Se almacena el registro del dia a dia
+  almacenarRegistroDia(este, data_este);
+  almacenarRegistroDia(norte, data_norte);
+  almacenarRegistroDia(noreste, data_noreste);
+  almacenarRegistroDia(noroeste, data_noroeste);
+  almacenarRegistroDia(oeste, data_oeste);
+  almacenarRegistroDia(sur, data_sur);
+  almacenarRegistroDia(sureste, data_sureste);
+  almacenarRegistroDia(suroeste, data_suroeste);
 
   return {
-    labels: labelsOrdenados,
-    direccion: { 0: data_este, 1: data_norte, 2: data_oeste, 3: data_sur }
+    labels: labelsNoRepetidos,
+    direccion: { 0: data_este, 1: data_norte, 2: data_noreste, 3: data_noroeste, 4: data_oeste, 5: data_sur, 6: data_sureste, 7: data_suroeste }
   };
 }
 
@@ -290,6 +307,45 @@ function compararFechas(a, b) {
   const fechaA = a.split('/').reverse().join('');
   const fechaB = b.split('/').reverse().join('');
   return fechaA.localeCompare(fechaB);
+}
+
+function extraerDias(data, labels) {
+  data.forEach(element => {
+    labels.push(element.dia);
+  });
+}
+
+function almacenarRegistroDia(data, array_store) {
+
+  // Se crea una variable temporal la cual se modificara para la obtencion de los registros
+  let temp_data = data;
+
+  labelsNoRepetidos.forEach(dia => {
+
+    let aux = false;
+
+    temp_data.forEach(element => {
+      if (dia == element.dia) {
+        array_store.push(element.prom_direccion_viento);
+        aux = true;
+        return;
+      }
+    });
+
+    if (!aux) {
+      array_store.push(0);
+    }
+
+  });
+}
+
+function almacenarRegistroHora(data, array_store, indice) {
+  const valor_encontrado = data.find(element => element['hora'] === calcularHora(indice));
+  if (valor_encontrado === undefined) {
+    array_store.push(0);
+  } else {
+    array_store.push(valor_encontrado.prom_direccion_viento);
+  }
 }
 
 app.use("/", router);
