b008a3f574abc6da2b6e2111255a170ed0fa8e5c
EstuardoSon <2986622050101@ingenieria.usac.edu.gt>
2023-10-20 22:52:09 -0600

    Actualizacion Base de datos

--- Practica 2/DB/DB.sql
+++ Practica 2/DB/DB.sql
@@ -2,8 +2,19 @@ drop database if exists arqui2practica2;
 create database if not exists arqui2practica2;
 use arqui2practica2;
 
+create table if not exists usuario(
+usuario varchar(20) primary key,
+nombre varchar(100),
+password varchar(100),
+edad int,
+altura float(2),
+peso float(2)
+);
+
 create table if not exists historialOxigeno(
 id int primary key auto_increment,
 porcentaje int,
+frecuencia int,
+usuario int,
 fecha datetime
 );
--- Proyecto 2/DB/DB.sql
+++ Proyecto 2/DB/DB.sql
@@ -2,8 +2,38 @@ drop database if exists arqui2practica2;
 create database if not exists arqui2practica2;
 use arqui2practica2;
 
+drop table if exists horarieSue; 
+create table horarioSue(
+id int primary key,
+dia varchar(10),
+horas varchar(20)
+);
+
+insert into horarioSue values(1,"Lunes","23:00-8:30");
+insert into horarioSue values(2,"Martes","23:00-8:30");
+insert into horarioSue values(3,"Miercoles","23:00-8:30");
+insert into horarioSue values(4,"Jueves","23:00-8:30");
+insert into horarioSue values(5,"Viernes","23:00-8:30");
+
+drop table if exists usuario;
+create table if not exists usuario(
+usuario varchar(20) primary key,
+nombre varchar(100),
+pass varchar(100),
+edad int,
+altura float(2),
+peso float(2),
+diabetes bool,
+cardiovascular bool,
+pulmonar bool,
+autoinmune bool
+);
+
+drop table if exists historialOxigeno;
 create table if not exists historialOxigeno(
 id int primary key auto_increment,
 porcentaje int,
+frecuencia int,
+usuario int,
 fecha datetime
 );
