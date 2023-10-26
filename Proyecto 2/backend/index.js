01f915a88ac50c79b61bb89fc9a3a1989077dd0c
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2023-10-26 14:54:42 -0600

    [ADD] Tabla con el historico de actividades

--- Proyecto 2/backend/index.js
+++ Proyecto 2/backend/index.js
@@ -27,7 +27,39 @@ app.use(morgan("dev"));
 
 // importe de libreria para la conexion a la base de datos
 const { getConnection } = require("./src/database");
-const RUTA_REQUEST = 'http://35.245.231.94:3000/';
+const RUTA_REQUEST = 'http://localhost:3000/';
+
+// Variable para determinar los rangos de actividades
+let rango_actividades = {
+  // Para problema pulmonar
+  0: {
+    intensa: [70, 85, 90],
+    normal: [50, 70, 88],
+    reposo: [60, 80, 88],
+    irregular_suenio: [60, 100, 88]
+  },
+  // Para problema cardiovascular
+  1: {
+    intensa: [70, 85, 90],
+    normal: [50, 70, 95],
+    reposo: [60, 80, 95],
+    irregular_suenio: [60, 100, 95]
+  },
+  // Para problema diabetes
+  2: {
+    intensa: [70, 85, 90],
+    normal: [50, 70, 95],
+    reposo: [60, 80, 95],
+    irregular_suenio: [60, 100, 95]
+  },
+  // Para problema autoinmune
+  3: {
+    intensa: [70, 250, 90],
+    normal: [50, 70, 95],
+    reposo: [60, 80, 95],
+    irregular_suenio: [60, 100, 95]
+  },
+};
 
 // Rutas
 app.get("/", async (req, res) => {
@@ -112,6 +144,33 @@ app.get("/informacion/:usuario/:fecha_desde/:fecha_hasta", async (req, res) => {
   }
 });
 
+app.get("/lista-actividades/:usuario/:fecha_desde/:fecha_hasta", async (req, res) => {
+
+  const fecha_desde = req.params.fecha_desde;
+  const fecha_hasta = req.params.fecha_hasta;
+  const usuario = req.params.usuario;
+
+  const conexion = getConnection();
+  const query = `SELECT ho.id, ho.porcentaje, ho.frecuencia, u.usuario, DATE_FORMAT(ho.fecha, '%d/%m/%Y %H:%i:%s') AS fecha,
+  CASE WHEN u.pulmonar = TRUE THEN 0 WHEN u.cardiovascular = TRUE THEN 1 WHEN u.diabetes = TRUE THEN 2 WHEN u.autoinmune = TRUE THEN 3 END AS indice,
+  CASE
+    WHEN (DAYOFWEEK(ho.fecha) = u.horarioSue + 1 AND HOUR(ho.fecha) >= 23) OR (DAYOFWEEK(ho.fecha) = (u.horarioSue + 2) AND TIME(ho.fecha) <= '08:30:00')
+    THEN True ELSE False
+  END AS es_horario_suenio
+  FROM historialOxigeno ho
+  INNER JOIN usuario u ON u.usuario = ho.usuario
+  WHERE u.usuario = ? AND ho.fecha >= ? AND ho.fecha < DATE_ADD(?, INTERVAL 1 DAY)
+  ORDER BY fecha DESC`;
+
+  try {
+    const [results, fields] = await conexion.query(query, [usuario, fecha_desde, fecha_hasta]);
+    res.json(formatearListaActividades(results));
+  } catch (error) {
+    res.status(500).json({ code: 500, message: error });
+  }
+
+});
+
 app.post("/registrar", async (req, res) => {
   const data = req.body;
   const conexion = getConnection();
@@ -184,6 +243,58 @@ function calcularHora(multiplicador) {
   return `${horas.toString().padStart(2, "0")}:${minutos
     .toString()
     .padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
+}
+
+function formatearListaActividades(result) {
+
+  let respuesta = "";
+  let actividad = "";
+  let color = "";
+
+  result.forEach(element => {
+
+    // Se analiza unicamente la irregularidad del suenio
+    if (element.es_horario_suenio) {
+      if ((element.frecuencia < rango_actividades[element.indice].irregular_suenio[0] || element.frecuencia > rango_actividades[element.indice].irregular_suenio[1]) &&
+        (element.porcentaje >= rango_actividades[element.indice].irregular_suenio[2])) {
+        actividad = "Irregularidad durante el sueño";
+        color = "Rojo";
+      } else {
+        actividad = "Normal durante el sueño";
+        color = "Cafe";
+      }
+    }
+    // Se analiza si la actividad fisica es intensa, normal, reposo
+    else {
+      if ((element.frecuencia > rango_actividades[element.indice].intensa[0] && element.frecuencia <= rango_actividades[element.indice].intensa[1]) &&
+        (element.porcentaje >= rango_actividades[element.indice].intensa[2])) {
+        actividad = "Actividad intensa";
+        color = "Anaranjado";
+      } else if ((element.frecuencia >= rango_actividades[element.indice].normal[0] && element.frecuencia <= rango_actividades[element.indice].normal[1]) &&
+        (element.porcentaje >= rango_actividades[element.indice].normal[2])) {
+        actividad = "Actividad normal";
+        color = "Verde";
+      } else if ((element.frecuencia >= rango_actividades[element.indice].reposo[0] && element.frecuencia <= rango_actividades[element.indice].reposo[1]) &&
+        (element.porcentaje >= rango_actividades[element.indice].reposo[2])) {
+        actividad = "Reposo";
+        color = "Azul";
+      } else {
+        actividad = "Irregularidad durante alguna actividad";
+        color = "Morado";
+      }
+    }
+
+    respuesta += element.usuario + "," +
+      element.id + "," +
+      element.fecha + "," +
+      actividad + "," +
+      element.porcentaje + "%," +
+      element.frecuencia + "," +
+      color + "," +
+      (element.es_horario_suenio ? 'Si' : 'No') + ";"
+  });
+
+  return respuesta;
 }
 
 const port = 3000;
